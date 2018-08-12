d7fb62855e03367347b67441c29bbd60937563b3
kunholee <gunho1020@gmail.com>
2018-08-12 14:43:52 +0900

    3-10. Contact List App- Contacts Routes and Contact Detail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 const App = () => (
   <div>
@@ -10,6 +10,16 @@ const App = () => (
 const Header = () => (
   <header>
     <h1>My Contacts</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contacts">Contacts</Link>
+        </li>
+      </ul>
+    </nav>
     <Route exact path="/" component={Welcome} />
     <Route path="/contacts" component={Contacts} />
   </header>
@@ -17,12 +27,29 @@ const Header = () => (
 
 const Welcome = () => <h2>Welcome to the best contacts app!</h2>;
 
-const Contacts = () => (
-  <ul>
-    <li>Lynn</li>
-    <li>Jisu Kumchi</li>
-    <li>Japan Guy</li>
-  </ul>
+const Contacts = ({ match }) => (
+  <Fragment>
+    <ul>
+      <li>
+        <Link to="/contacts/lynn">Lynn</Link>
+      </li>
+      <li>
+        <Link to="/contacts/jisu">Jisu Kumchi</Link>
+      </li>
+      <li>
+        <Link to="/contacts/japan">Japan Guy</Link>
+      </li>
+    </ul>
+    <Route
+      exact
+      path={`${match.url}`}
+      render={() => <h3>Please select a contact</h3>}
+    />
+    <Route path={`${match.url}/:contactName`} component={Contact} />
+  </Fragment>
 );
+
+const Contact = ({ match }) =>
+  `Your friend's name is ${match.params.contactName}`;
 
 export default App;
