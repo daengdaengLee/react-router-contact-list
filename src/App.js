4703a79ac7f47b616ed68452313bab096b33919e
kunholee <gunho1020@gmail.com>
2018-08-12 14:26:41 +0900

    3-9. Contact List App- Created Home page + Contacts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
@@ -9,7 +10,19 @@ const App = () => (
 const Header = () => (
   <header>
     <h1>My Contacts</h1>
+    <Route exact path="/" component={Welcome} />
+    <Route path="/contacts" component={Contacts} />
   </header>
+);
+
+const Welcome = () => <h2>Welcome to the best contacts app!</h2>;
+
+const Contacts = () => (
+  <ul>
+    <li>Lynn</li>
+    <li>Jisu Kumchi</li>
+    <li>Japan Guy</li>
+  </ul>
 );
 
 export default App;
